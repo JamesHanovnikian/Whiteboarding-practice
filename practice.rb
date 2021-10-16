facf53260058654e9d82cad0b8c9e73f24a4d963
YOUR NAME <jhanov@gmail.com>
2021-10-16 14:25:18 -0500

    first

---
+++ practice.rb
@@ -0,0 +1,24 @@
+# Given an array of numbers, write a function that returns a new array that contains all numbers from the original array that are less than 100.
+
+# Input: [99, 101, 88, 4, 2000, 50]
+# Output: [99, 88, 4, 50]
+
+# Make a variable holding new array
+# While loop
+# Through each iteration check to see if that index is under 100
+# If it is under 100, then add it to the new array.
+# Print the new array
+
+# def lownumbers(array)
+#   new_array = []
+#   i = 0
+#   while i < array.length
+#     if array[i] < 100
+#       new_array << array[i]
+#     end
+#     i = i + 1
+#   end
+#   new_array
+# end
+
+# p lownumbers([99, 101, 88, 4, 2000, 50])
