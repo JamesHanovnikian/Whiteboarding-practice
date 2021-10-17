1e6e73520a46df7ef4f358920519c2541f6e8af1
YOUR NAME <jhanov@gmail.com>
2021-10-17 11:12:09 -0500

    add single array problem

--- rubypointers.rb
+++ rubypointers.rb
@@ -26,3 +26,33 @@
 # end
 
 # p combination_letters(["a", "b", "c"], ["d", "e", "f", "g"])
+
+# Given ONE array of strings, return a new array that contains every combination of each string with every other string in the array.
+
+# Input: ["a", "b", "c", "d"]
+# Output: ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"]
+
+# Need a variable for the new array
+# Need a variable for index e1
+# While loop1 to loop through the array
+# while loop to loop through the second concatenated item
+# If index1 and index2 are NOT equal then
+#   add them up and push it to the new array
+
+def combo_letters(array)
+  new_array = []
+  i1 = 0
+  while i1 < array.length
+    i2 = 0
+    while i2 < array.length
+      if i1 != i2
+        new_array << array[i1] + array[i2]
+      end
+      i2 += 1
+    end
+    i1 += 1
+  end
+  new_array
+end
+
+p combo_letters(["a", "b", "c", "d"])
