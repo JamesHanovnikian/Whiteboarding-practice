e10fced1ecf958042e17922e3626641baddca213
YOUR NAME <jhanov@gmail.com>
2021-10-31 19:22:14 -0500

    add fizzbuzz solution

--- basicstring.rb
+++ basicstring.rb
@@ -134,3 +134,5 @@
 # end
 
 # p hamming("ABCDEFGL", "ABCDEFGP")
+
+
--- numbers.rb
+++ numbers.rb
@@ -18,3 +18,21 @@
 # If the number is divisible by 3, print out "FIZZ".
 # If the number is divisible by 5, print out "BUZZ".
 # If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+# def fizzbuzz(number)
+#   i = 1
+#   while i <= number
+#     if i % 3 == 0 && i % 5 == 0
+#       p "FIZZBUZZ"
+#     elsif i % 3 == 0
+#       p "FIZZ"
+#     elsif i % 5 == 0
+#       p "BUZZ"
+#     else
+#       p i
+#     end
+#     i += 1
+#   end
+# end
+
+# p fizzbuzz(99)
