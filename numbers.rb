93ab01a288b15de4159605457591c29b338b8d07
YOUR NAME <jhanov@gmail.com>
2021-10-31 20:22:27 -0500

    add multiples alog

--- numbers.rb
+++ numbers.rb
@@ -86,3 +86,21 @@
 # end
 
 # p leap_year?(2017)
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# def multiples(n)
+#   sum = 0
+#   i = n - 1
+#   while i >= 0
+#     if i % 3 == 0 || i % 5 == 0
+#       sum = sum + i
+#     end
+#     i -= 1
+#   end
+#   sum
+# end
+
+# p multiples(1000)
