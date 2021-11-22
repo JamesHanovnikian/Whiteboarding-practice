bca3e449370bd99b2c93a71c7b757af164565291
YOUR NAME <jhanov@gmail.com>
2021-11-22 15:22:43 -0600

    added complete the data algo

--- datatransformations.rb
+++ datatransformations.rb
@@ -90,3 +90,56 @@
 #     1 => ["A", "E"],
 #     2 => ["D", "G"],
 #   })
+
+def complete_data(posts, users)
+  posts.each do |post|
+    user_id = post[:submitted_by]
+    users.each do |user|
+      if user_id == user[:user_id]
+        username = user[:name]
+        post[:submitted_by] = username
+      end
+    end
+  end
+  posts
+end
+
+p complete_data ([
+                  { title: "Me Eating Pizza", submitted_by: 231, likes: 1549 },
+                  { title: "i never knew how cool i was until now", submitted_by: 989, likes: 3 },
+                  { title: "best selfie evar!!!", submitted_by: 111, likes: 1092 },
+                  { title: "Mondays are the worst", submitted_by: 403, likes: 644 },
+                ]), ([
+                  { user_id: 403, name: "Aunty Em" },
+                  { user_id: 231, name: "Joelle P." },
+                  { user_id: 989, name: "Lyndon Johnson" },
+                  { user_id: 111, name: "Patti Q." },
+                ])
+
+# Given an array of social media posts and an array of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
+
+# For example, given this array of posts (note that the submitted_by is an id number):
+
+# [
+# {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+# ]
+
+# And this array of users:
+
+# [
+# {user_id: 403, name: "Aunty Em"},
+# {user_id: 231, name: "Joelle P."},
+# {user_id: 989, name: "Lyndon Johnson"},
+# {user_id: 111, name: "Patti Q."},
+# ]
+
+# Return the array of posts as follows:
+
+# [
+# {title: 'Me Eating Pizza', submitted_by: "Joelle P.", likes: 1549},
+# {title: 'i never knew how cool i was until now', submitted_by: "Lyndon Johnson", likes: 3},
+# {title: 'best selfie evar!!!', submitted_by: "Patti Q.", likes: 1092},
+# {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}]
