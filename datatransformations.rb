4ae0d6688555074cf57af431628ae3a809e482b8
YOUR NAME <jhanov@gmail.com>
2021-11-22 14:33:00 -0600

    flip hash exercise

--- datatransformations.rb
+++ datatransformations.rb
@@ -40,3 +40,18 @@
 # end
 
 # p flatten_hash ({ "a" => 1, "b" => 2, "c" => 3, "d" => 4 })
+
+# Given a hash, create a new hash that has the keys and values switched.
+
+# Input: {"a" => 1, "b" => 2, "c" => 3}
+# Output: {1 => "a", 2 => "b", 3 => "c"}
+
+# def flip_hash(hash)
+#   new_hash = {}
+#   hash.each do |k, v|
+#     new_hash[v] = k
+#   end
+#   new_hash
+# end
+
+# p flip_hash({ "a" => 1, "b" => 2, "c" => 3 })
