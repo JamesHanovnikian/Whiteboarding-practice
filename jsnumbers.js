56fa5d3321b56920f441d92acab945661dd02e4c
YOUR NAME <jhanov@gmail.com>
2021-12-10 17:17:21 -0600

    fizzbuzz algo

--- jsnumbers.js
+++ jsnumbers.js
@@ -12,3 +12,28 @@
 // }
 
 // console.log(primeNumber(11));
+
+
+// Write a function that prints out every number from 1 to N, with the following exceptions:
+
+// If the number is divisible by 3, print out "FIZZ".
+// If the number is divisible by 5, print out "BUZZ".
+// If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+
+// function fizzBuzz(n) {
+//   let count = 0;
+//   while (count <= n) {
+//     count += 1;
+//     if (count % 3 === 0 && count % 5 === 0) {
+//       console.log("FIZZBUZZ");
+//     } else if (count % 5 === 0) {
+//       console.log("BUZZ");
+//     } else if (count % 3 === 0) {
+//       console.log("FIZZ");
+//     }
+//   }
+// }
+
+// console.log(fizzBuzz(15));
+
