89eecdb6ee418f86b36e5b71ee5efb6d3c76c1a5
YOUR NAME <jhanov@gmail.com>
2021-12-11 12:54:10 -0600

    leap year exercise

--- jsnumbers.js
+++ jsnumbers.js
@@ -98,3 +98,35 @@
 // Resulting in 9 steps. So for input n = 12, the return value would be 9.
 
 
+// Given a year, report if it is a leap year.
+
+// The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+// on every year that is evenly divisible by 4
+// except every year that is evenly divisible by 100
+// unless the year is also evenly divisible by 400
+// For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+// If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+
+// If statement 400 return false 
+// If statment 100 --> return 
+//  Else If divisible by 100 
+//  
+
+
+
+// function leapYear(year) {
+//   let leapYear = false;
+//   if (year % 100 === 0 && year % 400 !== 0) {
+//     leapYear = false;
+//   } else if (year % 4 === 0 && year % 400 === 0) {
+//     leapYear = true;
+//   } else if (year % 4 === 0) {
+//     leapYear = true;
+//   }
+//   return leapYear;
+// }
+// console.log(leapYear(2000));
+
