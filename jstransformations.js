a71fbcc2617425f28143f8e3ad4e986b5b551a8f
YOUR NAME <jhanov@gmail.com>
2022-01-04 13:13:13 -0600

    js practice

--- jstransformations.js
+++ jstransformations.js
@@ -25,3 +25,40 @@
 // }
 
 // console.log(etl(["a", "e", "i", "o", "u"], 1));
+
+
+// Given a hash, return a flat array containing all the hash’s keys and values.
+
+// Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
+// Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
+
+// function flatten(hash) {
+//   let array = [];
+
+
+//   for (let key in hash) {
+//     array.push(key);
+//     array.push(hash[key]);
+//   }
+//   return array;
+// }
+
+// console.log(flatten({ "a": 1, "b": 2, "c": 3, "d": 4 }));
+
+
+
+// Given a hash, create a new hash that has the keys and values switched.
+
+// Input: {"a" => 1, "b" => 2, "c" => 3}
+// Output: {1 => "a", 2 => "b", 3 => "c"}
+
+
+// function flipHash(object) {
+//   let newHash = {};
+//   for (let key in object) {
+//     newHash[object[key]] = key
+//   }
+//   return newHash;
+// }
+
+// console.log(flipHash({ "a": 1, "b": 2, "c": 3, "d": 4 }));
